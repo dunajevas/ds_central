86a9beced689651f3e289c49f74dc659e7858ec7
dunajevas <e.dunajevas@gmail.com>
2014-04-20 11:43:53 +0300

    some stuff with images

--- images.R
+++ images.R
@@ -1,20 +1,34 @@
+library(jpeg)
+library(png)
+library(ggplot2)
+library(jsonlite)
+library(e1071)
+out <- fromJSON("out.json")
 getImageFeatures <- function(link){
   cc <- try(download.file(link, destfile="/tmp/jpegtmp.img"), silent=TRUE)
   if(class(cc)!='try-error'){
-    if(grepl('\\.jpeg', link))
+    if(grepl('\\.jpeg|\\.bin|\\.jpg', link))
       jj <- readJPEG("/tmp/jpegtmp.img")
     if(grepl('\\.png', link))
       jj <- readPNG("/tmp/jpegtmp.img")
+    # convert to grayscale
+    # http://www.johndcook.com/blog/2009/08/24/algorithms-convert-color-grayscale/
+    # lightness method
+    if(is.array(jj))
+      jj <- apply(jj, 1:2, function(x) (min(x)+max(x))/2)
     list(x=c(jj), link)
   }
 }
-links <- unique(out$imglink)
 
-lx <- lapply(links, getImageFeatures)
-View(subset(out, imglink=='http://api.ning.com:80/files/UIcaShGmttF-CuRR4gptssh8PymhhbHM5udWmrSRBm2GXNRT8wvmUWMfPDA6ZHj6ROj2-kDcscXHdavyEb69BcC6xTUuy7*K/866209901.jpeg?width=64&height=64&crop=1%3A1'))
+lx <- lapply(unique(out$imglink), getImageFeatures)
+save(lx, file='lx.Rdata')
 not.null <- !sapply(lx,  function(item) is.null(item[['x']]))
+which.min(colSums(x))
 x <- matrix(0, ncol=sum(not.null), nrow=length(lx[[1]][['x']]))
-sapply(lx, function(x) (length(x[['x']])))
 x <- do.call(cbind, lapply(lx[not.null], "[[", "x"))
+fit <- svm(x=t(x), type='one-classification', cost=0.001)
+fit
+View(data.frame(sapply(lx[not.null], function(l) l[[2]]),
+                predict(fit)))
 
-
+predict(fit)
